9637f6b0d779384c43eaa17b9d1dad4f08102716
ChingYenShih <b02504036@ntu.edu.tw>
2018-06-14 00:49:36 +0800

    modify basic_train.py

--- basic_train.py
+++ basic_train.py
@@ -9,50 +9,20 @@ from model.net import network
 import utils
 import torch.nn as nn
 
-def train(net, optimizer, criterion, loader, epoch):
+def train(net, optimizer, criterion, loader):
     pbar = tqdm(iter(loader))
-    net.train()
     correct = 0
-    total_loss = 0
-    count = 0
     for x_batch, y_batch in pbar:
         x_batch, y_batch = x_batch.to(device) / 255.0, y_batch.to(device)
-
-        #optimizer.zero_grad()
-
         #pred = net(x_batch)
-        #loss = criterion(pred, y_batch)
-        #total_loss += loss.item()
         #_, pred_class = torch.max(pred, 1)
-        #correct += (pred_class ==y).sum().item()
-
-        #loss.backward()
-        #optimizer.step()
-
-        count += len(x_batch)
-        pbar.set_description('Epoch: {}; Avg loss: {:.4f}; Avg acc: {:.2f}%'.\
-            format(epoch + 1, total_loss / count, correct / count * 100))
-
-
 
 def valid(net, criterion, loader):
     pbar = tqdm(iter(loader))
-    net.eval()
-    correct = 0
-    total_loss = 0
-    count = 0
     for x_batch, y_batch in pbar:
         x_batch, y_batch = x_batch.to(device) / 255.0, y_batch.to(device)
         #pred = net(x_batch
-        #loss = criterion(pred, y_batch)
-        #total_loss += loss.item()
         #_, pred_class = torch.max(pred, 1)
-        #correct += (pred_class ==y).sum().item()
-
-        count += len(x_batch)
-
-        pbar.set_description('Validation stage: Avg loss: {:.4f}; Avg acc: {:.2f}%'.\
-            format(total_loss / count, correct / count * 100))
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = 'Basic model training process')
     parser.add_argument('-b', '--batch_size', type = int, default = 32, help = 'Set batch size')
@@ -69,8 +39,8 @@ if __name__ == '__main__':
 
     criterion = nn.CrossEntropyLoss()
     net = network().to(device)
-    #optimizer = torch.optim.Adam(net.parameters(), lr = 5e-5)
-    optimizer = 1
-    for epoch in range(50000):
-        train(net, optimizer, criterion, train_loader, epoch)
+    optimizer = torch.optim.Adam(net.parameters(), lr = 5e-5)
+
+    for i in range(50000):
+        train(net, optimizer, criterion, train_loader)
         valid(net, criterion, val_loader)
