7c027517b5ca44d86e4ec644fb07da4f865205e5
ChingYenShih <b02504036@ntu.edu.tw>
2018-06-13 22:00:39 +0800

    net

---
+++ model/net.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+import torch.utils.data as Data
+
+class network(nn.Module):
+    def __init__(self):
+        super(network, self).__init__()
+    def forward(self, x):
+        pass
