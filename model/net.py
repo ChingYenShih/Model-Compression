c963da28732590ef7a6198788611330a4b23c13c
blacktomato <s90169@gmail.com>
2018-06-16 02:55:03 +0800

    syntax error fixed

--- model/net.py
+++ model/net.py
@@ -26,7 +26,7 @@ class basic_vgg(nn.Module):
 class Maxout(nn.Module):
     def __init__(self, pool_size):
         super().__init__()
-        self._pool_size = pool_size)
+        self._pool_size = pool_size
     def forward(self, x):
         assert x.shape[-1] % self._pool_size == 0, \
             'Wrong input last dim size ({}) for Maxout({})'.format(x.shape[-1], self._pool_size)
