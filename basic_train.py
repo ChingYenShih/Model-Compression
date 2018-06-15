bcdca544a5a60b70b610d587e77562cb0d2656ec
ChungSL <b02901001@ntu.edu.tw>
2018-06-16 02:27:00 +0800

    triplet loss

--- basic_train.py
+++ basic_train.py
@@ -15,23 +15,24 @@ def train(net, optimizer, criterion, loader, epoch):
     correct = 0
     total_loss = 0
     count = 0
-    for x_batch, y_batch in pbar:
-        x_batch, y_batch = x_batch.to(device).float() / 255.0, y_batch.to(device)
+    for a_batch, p_batch, n_batch in pbar:
+        a_batch = a_batch.to(device).float() / 255.0
+        p_batch = p_batch.to(device).float() / 255.0
+        n_batch = n_batch.to(device).float() / 255.0
 
         optimizer.zero_grad()
 
-        pred = net(x_batch)
-        loss = criterion(pred, y_batch)
+        a_embedding = net(a_batch)
+        p_embedding = net(p_batch)
+        n_embedding = net(n_batch)
+        loss = criterion(a_embedding, p_embedding , n_embedding)
         loss.backward()
         optimizer.step()
 
         total_loss += loss.item()
-        _, pred_class = torch.max(pred, 1)
-        correct += (pred_class == y_batch).sum().item()
 
-        count += len(x_batch)
-        pbar.set_description('Epoch: {}; Avg loss: {:.4f}; Avg acc: {:.2f}%'.\
-            format(epoch + 1, total_loss / count, correct / count * 100))
+        count += len(a_batch)
+        pbar.set_description('Epoch: {}; Avg loss: {:.4f}'.format(epoch + 1, total_loss / count))
 
 def valid(net, criterion, loader):
     pbar = tqdm(iter(loader))
@@ -39,23 +40,22 @@ def valid(net, criterion, loader):
     correct = 0
     total_loss = 0
     count = 0
-    for x_batch, y_batch in pbar:
-        x_batch, y_batch = x_batch.to(device).float() / 255.0, y_batch.to(device)
- 
-        pred = net(x_batch)
+    for a_batch, p_batch, n_batch in pbar:
+        a_batch = a_batch.to(device).float() / 255.0
+        p_batch = p_batch.to(device).float() / 255.0
+        n_batch = n_batch.to(device).float() / 255.0
 
-        loss = criterion(pred, y_batch).detach()
-        
+        a_embedding = net(a_batch)
+        p_embedding = net(p_batch)
+        n_embedding = net(n_batch)
+        loss = criterion(a_embedding, p_embedding , n_embedding).detach()
+
         total_loss += loss.item()
-        _, pred_class = torch.max(pred, 1)
-        correct += (pred_class == y_batch).sum().item()
 
         count += len(x_batch)
+        pbar.set_description('Epoch: {}; Avg loss: {:.4f}'.format(epoch + 1, total_loss / count))
+    return total_loss/count
 
-        #pbar.set_description('Validation stage: Avg loss: {:.4f}; Avg acc: {:.2f}%'.\
-        #    format(total_loss / count, correct / count * 100))
-    acc = correct / count * 100
-    return acc
 class EarlyStop():
     def __init__(self, saved_model_path, patience = 10000, mode = 'max'):
         self.saved_model_path = saved_model_path
@@ -64,26 +64,26 @@ class EarlyStop():
         
         self.best = 0 if (self.mode == 'max') else np.Inf
         self.current_patience = 0
-    def run(self, acc, model):
-        condition = (acc > self.best) if (self.mode == 'max') else (acc <= self.best)
+    def run(self, value, model):
+        condition = (value > self.best) if (self.mode == 'max') else (value <= self.best)
         if(condition):
-            self.best = acc
+            self.best = value
             self.current_patience = 0
             with open('{}'.format(self.saved_model_path), 'wb') as f:
                 torch.save(model, f)
         else:
             self.current_patience += 1
             if(self.patience == self.current_patience):
-                print('Validation mean acc: {:.4f}, early stop patience: [{}/{}]'.\
-                      format(acc, self.current_patience,self.patience))
+                print('Validation mean value: {:.4f}, early stop patience: [{}/{}]'.\
+                      format(value, self.current_patience,self.patience))
                 return True
-        print('Validation mean acc: {:.2f}%, early stop[{}/{}], validation max acc: {:.2f}%'.\
-              format(acc, self.current_patience,self.patience, self.best))
+        print('Validation mean value: {:.2f}%, early stop[{}/{}], validation max value: {:.2f}%'.\
+              format(value, self.current_patience,self.patience, self.best))
         return False
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = 'Basic model training process')
-    parser.add_argument('-b', '--batch_size', type = int, default = 32, help = 'Set batch size')
+    parser.add_argument('-b', '--batch_size', type = int, default = 10, help = 'Set batch size')
     parser.add_argument('-d', '--device_id', type = int, default = 0, help = 'Set GPU device')
     parser.add_argument('-m', '--saved_model', default = 'saved_model/basic.model', help = 'Saved model path')
     args = parser.parse_args()
@@ -96,6 +96,7 @@ if __name__ == '__main__':
     y_val = torch.load('/mnt/data/r06942052/preproc_data/val_id.pt')
 
     #mapping new id
+    np.random.seed(69)
     bm_train = y_train.numpy()
     bm_val   = y_val.numpy()
     count_train = np.zeros((10177))
@@ -103,22 +104,52 @@ if __name__ == '__main__':
         count_train[i] = np.sum(bm_train == i)
     mapping = np.vstack((count_train.nonzero()[0].reshape(1, -1), np.arange(2360).reshape(1, -1)))
     mapping = mapping.astype('float')
-    for j in range(2360):
-        y_train[y_train == mapping[0, j]] = mapping[1, j]
-        y_val[    y_val == mapping[0, j]] = mapping[1, j]
 
-    train_set = Data.TensorDataset(x_train, y_train)
-    val_set = Data.TensorDataset(x_val, y_val)
+    anchor_train, positive_train, negative_train = [], [], []
+    anchor_val, positive_val, negative_val= [], [], []
+    for j in range(2360):
+        a_train = x_train[y_train == mapping[0, j]]
+        sample = np.random.choice(np.arange(len(x_train)-len(a_train)),
+                                         len(a_train), replace=False)
+        temp = np.arange(len(x_train))
+        np.random.shuffle(temp)
+        p_train = a_train[temp]
+        n_train = x_train[y_train != mapping[0, j]][sample]
+
+        a_val = x_val[y_val == mapping[0, j]]
+        sample = np.random.choice(np.arange(len(x_val)-len(a_val)), 
+                                       args.batch_size-len(a_val), replace=False)
+        temp = np.arange(len(x_val))
+        np.random.shuffle(temp)
+        p_val = a_val[temp]
+        n_val = x_val[y_val != mapping[0, j]][sample]
+
+        anchor_train.append(a_train)
+        positive_train.append(p_train)
+        negative_train.append(n_train)
+        anchor_val.append(a_val)
+        positive_val.append(p_val)
+        negative_val.append(n_val)
+
+    anchor_train = torch.cat(anchor_train)
+    positive_train = torch.cat(positive_train)
+    negative_train = torch.cat(negative_train)
+    anchor_val = torch.cat(anchor_val)
+    positive_val = torch.cat(positive_val)
+    negative_val = torch.cat(negative_val)
+
+    train_set = Data.TensorDataset(anchor_train, positive_train, negative_train)
+    val_set = Data.TensorDataset(anchor_val, positive_val, negative_val)
     train_loader = Data.DataLoader(dataset=train_set, batch_size=args.batch_size, shuffle=True) 
     val_loader   = Data.DataLoader(dataset=val_set, batch_size=args.batch_size, shuffle=False) 
 
-    criterion = nn.CrossEntropyLoss().to(device)
+    #criterion = nn.CrossEntropyLoss().to(device)
     #net = basic_vgg().to(device)
+    criterion = nn.TripletMarginLoss().to(device)
     net = facenet().to(device)
-    optimizer = torch.optim.Adam(net.parameters(), lr = 5e-5)
-    #optimizer = torch.optim.Adam(net.parameters(), lr=1e-4, betas=(0.5,0.999))
+    optimizer = torch.optim.Adam(net.parameters(), lr=1e-4, betas=(0.5,0.999))
 
-    earlystop = EarlyStop(saved_model_path = args.saved_model, patience = 10000, mode = 'max')
+    earlystop = EarlyStop(saved_model_path = args.saved_model, patience = 10000, mode = 'min')
 
     for epoch in range(50000):
         train(net, optimizer, criterion, train_loader, epoch)
