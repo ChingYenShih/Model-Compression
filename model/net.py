d5529b6a66b74fb3401d63bde0670f2611916344
ChungSL <b02901001@ntu.edu.tw>
2018-06-16 22:04:54 +0800

    remove the detach at l2 norm

--- model/net.py
+++ model/net.py
@@ -30,27 +30,27 @@ class Maxout(nn.Module):
     def forward(self, x):
         assert x.shape[-1] % self._pool_size == 0, \
             'Wrong input last dim size ({}) for Maxout({})'.format(x.shape[-1], self._pool_size)
-        m, i = x.view(x.shape[:-1][0], x.shape[-1]//self._pool_size, self_pool_size).max(-1)
+        m, i = x.view(x.shape[:-1][0], x.shape[-1]//self._pool_size, self._pool_size).max(-1)
         return m
 
 class facenet(nn.Module):
     def __init__(self):
         super(facenet, self).__init__()
         self.lrn    = nn.LocalResponseNorm(2)
-        self.maxpool= nn.MaxPool2d(3, stride=2)
-        self.conv1  = nn.Conv2d(  3,  64, (7,7), stride=2)
+        self.maxpool= nn.MaxPool2d(3, stride=2, padding=1)
+        self.conv1  = nn.Conv2d(  3,  64, (7,7), stride=2, padding = 3)
         self.conv2a = nn.Conv2d( 64,  64, (1,1), stride=1)
-        self.conv2  = nn.Conv2d( 64, 192, (3,3), stride=1)
+        self.conv2  = nn.Conv2d( 64, 192, (3,3), stride=1, padding = 1)
         self.conv3a = nn.Conv2d(192, 192, (1,1), stride=1)
-        self.conv3  = nn.Conv2d(192, 384, (3,3), stride=1)
+        self.conv3  = nn.Conv2d(192, 384, (3,3), stride=1, padding = 1)
         self.conv4a = nn.Conv2d(384, 384, (1,1), stride=1)
-        self.conv4  = nn.Conv2d(384, 256, (3,3), stride=1)
+        self.conv4  = nn.Conv2d(384, 256, (3,3), stride=1, padding = 1)
         self.conv5a = nn.Conv2d(256, 256, (1,1), stride=1)
-        self.conv5  = nn.Conv2d(256, 256, (3,3), stride=1)
+        self.conv5  = nn.Conv2d(256, 256, (3,3), stride=1, padding = 1)
         self.conv6a = nn.Conv2d(256, 256, (1,1), stride=1)
-        self.conv6  = nn.Conv2d(256, 256, (3,3), stride=1)
-        self.feature = nn.Sequential(
-                nn.Linear(4*2*256, 32*128*2, bias = True),
+        self.conv6  = nn.Conv2d(256, 256, (3,3), stride=1, padding = 1)
+        self.embedding = nn.Sequential(
+                nn.Linear(7*6*256, 32*128*2, bias = True),
                 nn.ReLU(),
                 Maxout(2),
                 nn.Linear( 32*128, 32*128*2, bias = True),
@@ -59,14 +59,14 @@ class facenet(nn.Module):
                 nn.Linear(32*128, 128),
             )
     def forward(self, x):
-        x = lrn(maxpool(F.relu(conv1(x))))
-        x = maxpool(lrn(F.relu(conv2(F.relu(conv2a(x))))))
-        x =     maxpool(F.relu(conv3(F.relu(conv3a(x)))))
-        x =             F.relu(conv4(F.relu(conv4a(x))))
-        x =             F.relu(conv5(F.relu(conv5a(x))))
-        x =     maxpool(F.relu(conv6(F.relu(conv6a(x)))))
+        x =                   self.lrn(self.maxpool(F.relu(self.conv1(x))))
+        x = self.maxpool(self.lrn(F.relu(self.conv2(F.relu(self.conv2a(x))))))
+        x =          self.maxpool(F.relu(self.conv3(F.relu(self.conv3a(x)))))
+        x =                       F.relu(self.conv4(F.relu(self.conv4a(x))))
+        x =                       F.relu(self.conv5(F.relu(self.conv5a(x))))
+        x =          self.maxpool(F.relu(self.conv6(F.relu(self.conv6a(x)))))
         x = x.view(x.size(0), -1)
-        x = self.features(x)
-        xn = torch.norm(x, p=2, dim=1).detach()
+        x = self.embedding(x)
+        xn = torch.norm(x, p=2, dim=1).view(-1,1)
         x = x.div(xn.expand_as(x))
         return x
