e791e6833cd9d39d3a0180df84e80f79288ba37d
ChungSL <b02901001@ntu.edu.tw>
2018-06-17 02:02:18 +0800

    crop faces

--- preproc.py
+++ preproc.py
@@ -2,6 +2,7 @@ import os
 import scipy.misc
 import numpy as np
 import argparse
+import skimage.transform
 import torch
 def save_as_tensor(mode = 'train'):
     '''
@@ -16,14 +17,18 @@ def save_as_tensor(mode = 'train'):
             image_name = line.split(' ')[0]
             id = torch.Tensor([int(line.split(' ')[1])]).long()
             image_path = os.path.join('dataset', mode, image_name)
-            img = np.transpose(scipy.misc.imread(image_path), (2, 0, 1)) # 3 x 218 x 178
-            img = torch.from_numpy(img).view(1, 3, 218, 178)
+            img = scipy.misc.imread(image_path) # 3 x 218 x 178
+            img = img[75:175, 40:140, :]
+            img = skimage.transform.resize(img, (220,220), 
+                        mode='constant', preserve_range=True).astype(np.uint8)
+            img = np.transpose(img, (2, 0, 1)) # 3 x 218 x 178
+            img = torch.from_numpy(img).view(1, 3, 220, 220)
 
             x.append(img)
             y.append(id)
 
 
-    torch.save(torch.cat(x), '/mnt/data/r06942052/preproc_data/{}_img.pt'.format(mode)) 
+    torch.save(torch.cat(x), '/mnt/data/r06942052/preproc_data/{}_crop_img.pt'.format(mode)) 
     torch.save(torch.cat(y), '/mnt/data/r06942052/preproc_data/{}_id.pt'.format(mode))
     
 if __name__ == '__main__':
